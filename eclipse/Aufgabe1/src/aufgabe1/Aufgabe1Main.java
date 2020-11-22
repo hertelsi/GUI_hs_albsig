d0ab05d99077c1dbed6346009afcb4dd16988b88
hertelsi <hertelsi@hs-albsig.de>
2020-11-22 10:25:54 +0100

    UML / Kommentare

--- eclipse/Aufgabe1/src/aufgabe1/Aufgabe1Main.java
+++ eclipse/Aufgabe1/src/aufgabe1/Aufgabe1Main.java
@@ -2,10 +2,10 @@ package aufgabe1;
 public class Aufgabe1Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Aufgabe1 w = Aufgabe1.createNewWindow();
 		w.open();
 
-	}// end method main()
+	}
 
-} // end Class Beispiel3Main
+} 
