07bfe654a3f9b1360378b58cb24764c1c294c43d
Julian Gombold <juliangombold2000@gmail.com>
2020-12-21 17:29:32 +0100

    Added remove functionality

--- Aufgabe2/Einkaufsliste/app/src/main/java/com/example/einkaufsliste/ui/lists/BuyingListsAdapter.java
+++ Aufgabe2/Einkaufsliste/app/src/main/java/com/example/einkaufsliste/ui/lists/BuyingListsAdapter.java
@@ -1,6 +1,8 @@
 package com.example.einkaufsliste.ui.lists;
 
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -38,6 +40,26 @@ public class BuyingListsAdapter extends RecyclerView.Adapter<BuyingListsAdapter.
         String name = buyingLists.get(position).getText();
         holder.button.setText(name);
 
+        holder.button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                final int DRAWABLE_RIGHT = 2;
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+
+                    int padding = holder.button.getPaddingRight();
+                    int leftXDrawable = (int) holder.button.getRight() - holder.button.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width() - padding;
+                    int rightXDrawable = (int) holder.button.getRight() - padding;
+                    if ((leftXDrawable <= event.getRawX()) && (event.getRawX() <= rightXDrawable)){
+                        //click on rightdrawable
+                        holder.remove(position);
+                        return true;
+                    } else {
+                        // TODO functionality to navigate to selected buyinglist
+                    }
+                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -49,20 +71,20 @@ public class BuyingListsAdapter extends RecyclerView.Adapter<BuyingListsAdapter.
         private View layout;
         private Button button;
 
-        public FirstViewHolder(View view){
+        public FirstViewHolder(View view) {
             super(view);
             layout = view;
             button = view.findViewById(R.id.buyinglist_item_button);
         }
 
-        public void add(int position, String name){
-            buyingLists.add(position,new BuyingList(name, new GregorianCalendar().getTime()));
+        public void add(int position, String name) {
+            buyingLists.add(position, new BuyingList(name, new GregorianCalendar().getTime()));
 
             notifyItemInserted(position);
         }
-        public void remove(int position, String item){
-            buyingLists.remove(item);
 
+        public void remove(int position) {
+            buyingLists.remove(position);
             notifyItemRemoved(position);
         }
 
